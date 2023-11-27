32f3b4c8a47688facafbad52101b296ef7e0b0ea
SadykowAzatbek <sadykowazatbek@gmail.com>
2023-11-27 15:36:08 +0600

    Компонент Home дополнен, добавлена новая секция и футер

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,36 +1,58 @@
 import bakery from '../../assets/bakery.jpg';
+import bakery2 from '../../assets/image.png';
 const Home = () => {
   return (
-    <div className="mt-5 bg-light rounded-3 p-3">
-      <h2 className="text-center fs-3">
-        The team
-        <span className="text-success fs-2"> Travelulu </span>
-        local bakery
-      </h2>
-      <div className="d-flex mt-4">
-        <div>
-          <p>
-            The sun was setting over the horizon, casting a warm glow across the sky. As the day
-            came to an end, the sounds of nature filled the air, from the chirping of birds to the
-            rustling of leaves in the gentle breeze. It was a peaceful scene, one that made you want
-            to pause and take it all in. The colors of the sky shifted from orange to pink to purple,
-            creating a mesmerizing display that seemed to stretch on for miles. In that moment,
-            everything felt right in the world. The worries and stresses of the day melted away,
-            leaving only a sense of calm and contentment.
-          </p>
-          It was a reminder of the beauty that surrounds
-          us, even in the midst of chaos. As the last rays of sunlight disappeared, the stars began to
-          emerge, twinkling in the darkening sky. It was a perfect ending to a perfect day, and it
-          filled you with gratitude for the simple pleasures in life. In that moment, you felt a
-          deep connection to the world around you, and a sense of awe at the wonders of nature.
-          And as you turned to head back home, you carried that feeling with you, knowing that
-          you would always have moments like these to look forward to.
+    <>
+      <div className="mt-5 bg-light rounded-3 p-3">
+        <h2 className="text-center fs-3">
+          The team
+          <span className="text-success fs-2"> Travelulu </span>
+          local bakery
+        </h2>
+        <div className="d-flex mt-4">
+          <div>
+            <p>
+              The sun was setting over the horizon, casting a warm glow across the sky. As the day
+              came to an end, the sounds of nature filled the air, from the chirping of birds to the
+              rustling of leaves in the gentle breeze. It was a peaceful scene, one that made you want
+              to pause and take it all in. The colors of the sky shifted from orange to pink to purple,
+              creating a mesmerizing display that seemed to stretch on for miles. In that moment,
+              everything felt right in the world. The worries and stresses of the day melted away,
+              leaving only a sense of calm and contentment.
+            </p>
+            It was a reminder of the beauty that surrounds
+            us, even in the midst of chaos. As the last rays of sunlight disappeared, the stars began to
+            emerge, twinkling in the darkening sky. It was a perfect ending to a perfect day, and it
+            filled you with gratitude for the simple pleasures in life. In that moment, you felt a
+            deep connection to the world around you, and a sense of awe at the wonders of nature.
+            And as you turned to head back home, you carried that feeling with you, knowing that
+            you would always have moments like these to look forward to.
+          </div>
+          <div>
+            <img src={bakery} alt="bakery" />
+          </div>
         </div>
-        <div>
-          <img src={bakery} alt="bakery" />
+        <div className="d-flex mt-4">
+          <img src={bakery2} alt="bakery" />
+          <div>
+            <p>
+              Casting a warm glow across the sky. As the day
+              came to an end, the sounds of nature filled the air, from the chirping of birds to the
+              rustling of leaves he sky shifted from orange to pink to purple,
+              creating a mesmerizing display that seemed to stretch on for miles. In that moment,
+              everything felt right in the world. The worries and stresses of the day melted away,
+              leaving only a sense of calm and contentment.
+            </p>
+            us, even in the midst of chaos. As the last rays of sunlight disappeared, the stars began to
+            emerge, twinkling in the darkening sky. It was a perfect ending to a perfect day, and it
+          </div>
         </div>
       </div>
-    </div>
+      <footer className="d-flex justify-content-between bg-light p-3 rounded-3 mt-5">
+        <a href="#" className="opacity-75">@Travelulu</a>
+        <div className="opacity-75">@$travelulu-travelulu.com</div>
+      </footer>
+    </>
   );
 };
 
